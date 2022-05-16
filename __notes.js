ee8a3e0dab20e18c05817ce2cee81b017fcc2a12
NABIL <johayeriqbal@gmail.com>
2022-05-17 05:29:07 +0600

    header and carousel added

---
+++ __notes.js
@@ -0,0 +1,5 @@
+/**
+ * tailwind
+ * react rowter
+ *  
+*/
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Carousel from './components/Pages/Home/Carousel/Carousel';
+import Header from './components/Pages/Shared/Header/Header';
+
+
+function App() {
+  return (
+    <div className="">
+      <Header></Header>
+      <Carousel></Carousel>
+    </div>
+  );
+}
+
+export default App;
