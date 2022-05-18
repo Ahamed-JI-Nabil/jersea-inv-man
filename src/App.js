ccd2aeeb69252c09805bf1df197b9a0ef6334293
NABIL <johayeriqbal@gmail.com>
2022-05-19 00:07:24 +0600

    requre auth added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Home from './components/Pages/Home/Home';
 import Login from '../src/components/Pages/Login/Login/Login'
 import Header from './components/Pages/Shared/Header/Header';
 import Register from './components/Pages/Login/Register/Register';
+import Inventory from './components/Pages/Inventory/Inventory';
+import CheckOut from './components/Pages/CheckOut/CheckOut';
+import RequireAuth from './components/Pages/Login/RequreAuth/RequreAuth';
 
 
 function App() {
@@ -15,7 +18,18 @@ function App() {
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='/inventory' element={
+          <RequireAuth>
+            <Inventory></Inventory>
+          </RequireAuth>
+        }></Route>
+        <Route path='/checkout' element={
+          <RequireAuth>
+            <CheckOut></CheckOut>
+          </RequireAuth>
+        }></Route>
       </Routes>
+
     </div>
   );
 }
---
+++ src/components/Pages/Inventory/Inventory.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import InventoryItems from '../Home/InventoryItems/InventoryItems';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h1> This is inventory section!!!</h1>
+            <InventoryItems></InventoryItems>
+        </div>
+    );
+};
+
+export default Inventory;
