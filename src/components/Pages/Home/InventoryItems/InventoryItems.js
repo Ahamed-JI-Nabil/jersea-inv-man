028774c05535ac061ed350c81a81b0eecfb10fda
NABIL <johayeriqbal@gmail.com>
2022-06-01 04:45:24 +0600

    responsive inventory items page

--- src/components/Pages/Home/InventoryItems/InventoryItems.js
+++ src/components/Pages/Home/InventoryItems/InventoryItems.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import useInvItems from '../../../../hooks/useInvItems';
-import BgImg from '../../Shared/BgImg/BgImg';
+import inventoryimage from '../../../../images/inventory item image.jpg'
 import SingleInvItem from './SingleInvItem/SingleInvItem';
 
 const InventoryItems = () => {
@@ -10,8 +10,8 @@ const InventoryItems = () => {
 
     return (
         <div>
-            <BgImg></BgImg>
-            <div className='grid grid-cols-4 m-6 p-4 px-16'>
+            <img src={inventoryimage} alt="" />
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 m-6 p-4 px-16'>
                 {
                     invItems.map(invItem => <SingleInvItem
                         key={invItem._id}
